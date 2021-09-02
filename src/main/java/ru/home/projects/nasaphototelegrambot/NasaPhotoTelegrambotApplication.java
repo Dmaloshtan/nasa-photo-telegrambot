67c38ee3f6e0de5a07599baae82101dae8207589
Dima Maloshtan <maloshtanda@gmail.com>
2021-09-02 22:49:13 +0300

    Create springboot project

---
+++ src/main/java/ru/home/projects/nasaphototelegrambot/NasaPhotoTelegrambotApplication.java
@@ -0,0 +1,13 @@
+package ru.home.projects.nasaphototelegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NasaPhotoTelegrambotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NasaPhotoTelegrambotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/home/projects/nasaphototelegrambot/NasaPhotoTelegrambotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.home.projects.nasaphototelegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NasaPhotoTelegrambotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
