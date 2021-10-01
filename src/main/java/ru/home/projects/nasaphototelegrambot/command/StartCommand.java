a43062d5ac1351a13249bfba87626f4aa0590985
Dima Maloshtan <maloshtanda@gmail.com>
2021-10-01 23:21:22 +0300

    Add ReplyKeyboardMarkup

--- src/main/java/ru/home/projects/nasaphototelegrambot/command/StartCommand.java
+++ src/main/java/ru/home/projects/nasaphototelegrambot/command/StartCommand.java
@@ -2,11 +2,16 @@ package ru.home.projects.nasaphototelegrambot.command;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import ru.home.projects.nasaphototelegrambot.repository.entity.TelegramUser;
 import ru.home.projects.nasaphototelegrambot.service.SendBotMessageService;
 import ru.home.projects.nasaphototelegrambot.service.TelegramUserService;
 
-public class StartCommand implements Command{
+import java.util.ArrayList;
+import java.util.List;
+
+public class StartCommand implements Command {
 
 
     private final SendBotMessageService messageService;
@@ -39,7 +44,21 @@ public class StartCommand implements Command{
                 }
         );
 
-
-    messageService.sendMessage(update.getMessage().getChatId().toString(), START_MESSAGE);
+        ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
+        List<KeyboardRow> keyboard = new ArrayList<>();
+        KeyboardRow row = new KeyboardRow();
+        row.add("Получить фото дня");
+        keyboard.add(row);
+        row = new KeyboardRow();
+        row.add("Получить фото c марсохода");
+        keyboard.add(row);
+        row = new KeyboardRow();
+        row.add("Информация о подписке на рассылку");
+        keyboard.add(row);
+        row = new KeyboardRow();
+        row.add("Помощь");
+        keyboard.add(row);
+        keyboardMarkup.setKeyboard(keyboard);
+        messageService.sendMessage(update.getMessage().getChatId().toString(), START_MESSAGE, keyboardMarkup);
     }
 }
--- src/main/java/ru/home/projects/nasaphototelegrambot/service/SendBotMessageService.java
+++ src/main/java/ru/home/projects/nasaphototelegrambot/service/SendBotMessageService.java
@@ -1,6 +1,9 @@
 package ru.home.projects.nasaphototelegrambot.service;
 
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+
 public interface SendBotMessageService {
 
     void sendMessage(String chatId, String message);
+    void sendMessage(String chatId, String message, ReplyKeyboardMarkup keyboardMarkup);
 }
